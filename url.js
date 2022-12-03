1854d0ac1462cc1df685274af45c9f4b4f4703e2
Anna111111111 <119248485+Anna111111111@users.noreply.github.com>
2022-12-03 16:57:06 +0200

    Update url.js

--- url.js
+++ url.js
@@ -1,5 +1,5 @@
 module.exports = (data) => {
-    data.url = "https://dev-bot-platform.corezoid.com/webhooks/get-weather?city=";
+    data.url1 = "https://dev-bot-platform.corezoid.com/webhooks/get-weather?city=";
 
     return data;
 };
